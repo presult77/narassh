f0e879f81fcfe0834c49e035c9183c97c3c1fdc9
Ridho <presult77@gmail.com>
2022-11-28 00:25:52 +0700

    fixdelssh

--- ssh/delssh.sh
+++ ssh/delssh.sh
@@ -17,7 +17,8 @@ bash member
 read -p "Username SSH to Delete : " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
-        userdel $Pengguna
+        userdel -f $Pengguna
+        service openssh restart
         echo -e "Username $Pengguna Telah Di Hapus"
 else
         echo -e "Failure: Username $Pengguna Tidak Ada CoK"
