cd5d4cb5c8a1b62133d6f5d85b8164a431db94d4
Ridho <presult77@gmail.com>
2022-12-11 12:46:24 +0700

    update

--- ssh/addssh.sh
+++ ssh/addssh.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 # ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 domain=$(cat /root/scdomain)
@@ -29,37 +19,38 @@ expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
+clear
 echo -e ""
-echo -e "═══════════════════════" | lolcat
+echo -e "═══════════════════════"
 echo -e "Informasi SSH Premium"
-echo -e "═══════════════════════" | lolcat
-echo -e "Hostname    : $domain" | lolcat
-echo -e "Username    : $Login" | lolcat
-echo -e "Password    : $Pass" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "OpenSSH     : 22" | lolcat
-echo -e "Dropbear    : 443, 109, 143" | lolcat
-echo -e "SSL/TLS     :$ssl" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "OHP SSH     : 8181" | lolcat
-echo -e "OHP Dropbear: 8282" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "SSH Ws SSL  : 443" | lolcat
-echo -e "SSH Ws HTTP : 80" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "BadVPN      : 7100,7200,7300" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "PAYLOAD WS HTTP" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "PAYLOAD WS SSL" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "GET wss://bug.com/ HTTP/1.1[crlf]Host: ${domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]" | lolcat
-echo -e "═══════════════════════" | lolcat
-echo -e "Created     : $hariini" | lolcat
-echo -e "Expired     : $expi" | lolcat
-echo -e "═══════════════════════" | lolcat
+echo -e "═══════════════════════"
+echo -e "Hostname    : $domain"
+echo -e "Username    : $Login"
+echo -e "Password    : $Pass"
+echo -e "═══════════════════════"
+echo -e "OpenSSH     : 22"
+echo -e "Dropbear    : 443, 109, 143"
+echo -e "SSL/TLS     :$ssl"
+echo -e "═══════════════════════"
+echo -e "OHP SSH     : 8181"
+echo -e "OHP Dropbear: 8282"
+echo -e "═══════════════════════"
+echo -e "SSH Ws SSL  : 443"
+echo -e "SSH Ws HTTP : 80"
+echo -e "═══════════════════════"
+echo -e "BadVPN      : 7100,7200,7300"
+echo -e "═══════════════════════"
+echo -e "PAYLOAD WS HTTP"
+echo -e "═══════════════════════"
+echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "═══════════════════════"
+echo -e "PAYLOAD WS SSL"
+echo -e "═══════════════════════"
+echo -e "GET wss://bug.com/ HTTP/1.1[crlf]Host: ${domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "═══════════════════════"
+echo -e "Created     : $hariini"
+echo -e "Expired     : $expi"
+echo -e "═══════════════════════"
 sleep 2
 systemctl restart ssh-ohp
 systemctl restart dropbear-ohp
--- ssh/restart.sh
+++ ssh/restart.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 # ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
 clear
 echo -e ""
 echo -e "Starting Restart All Service"
 sleep 2
-systemctl stop ws-tls
+systemctl stop ssh-ws
 systemctl start sslh
 systemctl restart sslh
 /etc/init.d/sslh start
 /etc/init.d/sslh restart
-systemctl restart ws-tls
-systemctl restart ws-nontls
+systemctl restart ssh-ws
 systemctl restart ssh-ohp
 systemctl restart dropbear-ohp
-systemctl restart openvpn-ohp
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
 /etc/init.d/sslh restart
 /etc/init.d/stunnel5 restart
-/etc/init.d/stunnel4 restart
 /etc/init.d/fail2ban restart
 /etc/init.d/cron restart
 /etc/init.d/nginx restart
